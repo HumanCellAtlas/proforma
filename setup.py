f4689c7fa96bfa0831615d949c4905593e2a38e5
Sam Pierson <sam@sampierson.com>
2018-02-13 10:56:38 -0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
 
 setup(name='proforma',
-      version='0.1.0',
+      version='0.2.0',
       description='Simple library for setting up AWS Infrastructure.',
       url='http://github.com/HumanCellAtlas/proforma',
       author='Sam Pierson',
